f2761ef310516c5ab5272f70e871dfa08a99836f
GroovyHooked <dafrenchie2002@yahoo.fr>
2024-03-19 14:43:52 +0100

    Update database scripts with users table

--- utils/create_table.js
+++ utils/create_table.js
@@ -7,11 +7,13 @@ const db = new sqlite3.Database('./database/motus.db', (err) => {
     console.log('Connected to the motus database.');
   });
 
-const sql = `CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT, difficulty TEXT)`;
+//const sql1 = `CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT, difficulty TEXT)`;
+const sql2 = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, password TEXT, bestScore INTEGER)`;
 
-db.run(sql, (err) => {
+db.run(sql2, (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Created crypto table.');
+    console.log('Created words table.');
+    //console.log('Created users table.');
 })
--- utils/drop_table.js
+++ utils/drop_table.js
@@ -7,11 +7,13 @@ const db = new sqlite3.Database('./database/motus.db', (err) => {
     console.log('Connected to the motus database.');
   });
 
-const sql = `DROP TABLE IF EXISTS words`;
+//const sql1 = `DROP TABLE IF EXISTS words`;
+const sql2 = `DROP TABLE IF EXISTS users`;
 
-db.run(sql, (err) => {
+db.run(sql2, (err) => {
     if (err) {
         console.error(err.message);
     }
     console.log('Dropped words table.');
+    //console.log('Dropped users table.');
 })
