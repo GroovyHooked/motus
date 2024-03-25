27cbef291f739f69c6aa76bddc64bc62f3316e2b
GroovyHooked <dafrenchie2002@yahoo.fr>
2024-03-25 15:22:42 +0100

    Delete unused lines of code

--- utils/create_table.js
+++ utils/create_table.js
@@ -7,13 +7,11 @@ const db = new sqlite3.Database('./database/motus.db', (err) => {
     console.log('Connected to the motus database.');
   });
 
-//const sql1 = `CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT, difficulty TEXT)`;
-const sql2 = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, password TEXT, bestScore INTEGER)`;
+const sql = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, password TEXT, bestScore INTEGER)`;
 
-db.run(sql2, (err) => {
+db.run(sql, (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Created words table.');
-    //console.log('Created users table.');
+    console.log('Created users table.');
 })
