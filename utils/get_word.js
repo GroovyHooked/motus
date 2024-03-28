1133b2f010bc6c57433d0ced6ba129a93f8ef21c
GroovyHooked <dafrenchie2002@yahoo.fr>
2024-03-28 10:41:36 +0100

    Refacto

--- utils/get_word.js
+++ utils/get_word.js
@@ -1,14 +1,13 @@
+const fetch = require('node-fetch');
 
 async function getWord(size = 5) {
     return new Promise((resolve, reject) => {
-        import('node-fetch').then(({ default: fetch }) => {
-            fetch(`https://trouve-mot.fr/api/size/${size}`)
-                .then((response) => response.json())
-                .then((words) => {
-                    resolve(words[0]?.name)
-                })
-                .catch((error) => reject(error));
-        });
+        fetch(`https://trouve-mot.fr/api/size/${size}`)
+            .then((response) => response.json())
+            .then((words) => {
+                resolve(words[0]?.name)
+            })
+            .catch((error) => reject(error));
     });
 }
 
