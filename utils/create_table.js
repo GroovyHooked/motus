311551d961144cc5673639b61887d11a72d5a713
GroovyHooked <dafrenchie2002@yahoo.fr>
2024-05-06 11:15:20 +0200

    Database path modifi

--- utils/create_table.js
+++ utils/create_table.js
@@ -1,10 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./database/motus.db', (err) => {
+const db = new sqlite3.Database(`${__dirname}/database/motus.db`, (err) => {
     if (err) {
-        console.error(err.message);
+        console.error('Database error:', err.message);
+    } else {    
+        console.log('Connected to the motus database.');
     }
-    console.log('Connected to the motus database.');
   });
 
 const sql = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, password TEXT, bestScore INTEGER)`;
